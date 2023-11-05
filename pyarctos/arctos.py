8436396f6f51fd67404c3de7d251144a4a01a5dc
fiatlux20 <harlanrhwilliams@gmail.com>
2023-11-04 19:20:18 -0500

    hmm

--- pyarctos/arctos.py
+++ pyarctos/arctos.py
@@ -14,10 +14,17 @@ def set_api_key(key: str):
     _API_KEY = key
 
 
-def catalog(query: str):
+def catalog(query: str, start=0: int):
     if _API_KEY is None:
         raise 'no api key'
-    params = { 'api_key': _API_KEY, 'method': 'getCatalogData', 'scientific_name': query }
+    params = {
+        'api_key': _API_KEY,
+        'method': 'getCatalogData',
+        'queryformat': 'struct',
+        'scientific_name': query,
+        'start': start,
+        'limit': 10
+    }
     headers = {'user-agent': 'PyArctos/0.0.1'}
     r = requests.get(f'{_ARCTOS_URL}/catalog.cfg', params=params)
     if not r.status_code == requests.codes.ok:
